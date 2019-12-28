762c9c1928b86e25c6e1df3e3f4e2c250d35458a
shakiz <activities335@gmail.com>
2019-12-28 14:41:43 +0600

    data binding

--- app/src/main/java/com/shakil/databinding/MainActivity.java
+++ app/src/main/java/com/shakil/databinding/MainActivity.java
@@ -1,14 +1,23 @@
 package com.shakil.databinding;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.shakil.databinding.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        activityMainBinding.tv1.setText("Whooooo !!! Its working");
+        activityMainBinding.tv2.setText("Whooooo !!! Its working");
+        activityMainBinding.tv3.setText("Whooooo !!! Its working");
     }
 }
